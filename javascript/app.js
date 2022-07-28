6e0e803df2355398a1e7db6bdc33532665e7182b
kntdev <srodriguez.dev@gmail.com>
2022-07-27 22:44:20 -0300

    asdads

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,44 @@
-const time1 = document.getElementById("timer1");
-const time2 = document.getElementById("timer2");
-const time3 = document.getElementById("timer3");
-const btnapp = document.getElementById("btn-app");
+const timer = document.getElementById("timer");
 
-const startingMinutes = 25;
+const btnapp = document.getElementById("btn-app");
+const sound = document.getElementById("sound");
+const btnpomodoro = document.getElementById("btn-pomodoro");
+const btnshort = document.getElementById("btn-short");
+const btnlong = document.getElementById("btn-long");
+
+var valor = 25;
+
+btnshort.addEventListener("click",(valor)=>{
+  valor = 5;
+  return  
+});
+btnlong.addEventListener("click",(valor)=>{
+  valor = 15;
+  return  
+});
+btnpomodoro.addEventListener("click",(valor)=>{
+  valor = 25;
+  return  
+});
+
+startingMinutes = valor;
+
 let time = startingMinutes * 60;
 
-
 function updateCountdown() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  cuenta = `${minutes}:${seconds}`;
-  time--;
-  time1.innerHTML = cuenta;
-  if(cuenta == '0:00'){
-    console.log("aguante el rojo")
-    clearInterval(caca);
-  }
+const minutes = Math.floor(time / 60);
+let seconds = time % 60;
+
+seconds = seconds < 10 ? '0' + seconds : seconds;
+cuenta = `${minutes}:${seconds}`;
+time--;
+timer.innerHTML = cuenta;
+if(cuenta == '0:00'){
+  sound.innerHTML='<audio src="./assets/notification.mp3" autoplay></audio>';
+  clearInterval(temporizador);
+}
 }
 
-btnapp.addEventListener("click",()=>{setInterval(updateCountdown, 1000)})
+btnapp.addEventListener("click",()=>{
+temporizador = setInterval(updateCountdown, 1000);
+});
