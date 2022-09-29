440a02c9c9bb1a3df035e9469e674b344dd3cddd
kntdev <srodriguez.dev@gmail.com>
2022-09-29 08:53:03 -0300

    Se añadio la funcionalidad de pomodoro timer, falta la app de tareas.

--- javascript/app.js
+++ javascript/app.js
@@ -1,50 +1,160 @@
-const timer = document.getElementById("timer");
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  sessions: 0,
+};
+let interval;
 
-const btnapp = document.getElementById("btn-app");
-const sound = document.getElementById("sound");
-const btnpomodoro = document.getElementById("btn-pomodoro");
-const btnshort = document.getElementById("btn-short");
-const btnlong = document.getElementById("btn-long");
 
-function valor(a){
-  console.log(a)
-  return a
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+function handleMode(event) {
+const { mode } = event.target.dataset;
+
+if (!mode) return;
+
+switchMode(mode);
+stopTimer();
 }
 
- const tPomodoro = valor(25);
- const tLong = valor(15);
- const tShort = valor(5);
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
 
-btnpomodoro.addEventListener("click",()=>{
-  startingMinutes = tPomodoro;
-});
-btnshort.addEventListener("click",()=>{
-  startingMinutes = tShort;
-});
-btnlong.addEventListener("click",()=>{
-  startingMinutes = tLong;
-});
+  document
+    .querySelectorAll('button[data-mode]')
+    .forEach(e => e.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+  // document.body.style.backgroundColor = `var(--${mode})`;
+  document
+  .getElementById('js-progress')
+  .setAttribute('max', timer.remainingTime.total);
 
-startingMinutes = null;
-
-timer.innerHTML = startingMinutes;
-
-let time = startingMinutes * 60;
-
-function updateCountdown() {
-let minutes = Math.floor(time / 60);
-let seconds = time % 60;
-
-seconds = seconds < 10 ? '0' + seconds : seconds;
-cuenta = `${minutes}:${seconds}`;
-time--;
-timer.innerHTML = cuenta;
-if(cuenta == '0:00'){
-  sound.innerHTML='<audio src="./assets/notification.mp3" autoplay></audio>';
-  clearInterval(temporizador);
-}
+  updateClock();
 }
 
-btnapp.addEventListener("click",()=>{
-temporizador = setInterval(updateCountdown, 1000);
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+  const min = document.getElementById('js-minutes');
+  const sec = document.getElementById('js-seconds');
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById('js-progress');
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+}
+
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+  
+  if (timer.mode === 'pomodoro') timer.sessions++;
+
+  mainButton.dataset.action = 'stop';
+  mainButton.textContent = 'stop';
+  mainButton.classList.add('active');
+
+
+  interval = setInterval(function() {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+
+      switch (timer.mode) {
+          case 'pomodoro':
+            if (timer.sessions % timer.longBreakInterval === 0) {
+              switchMode('longBreak');
+            } else {
+              switchMode('shortBreak');
+            }
+            break;
+          default:
+            switchMode('pomodoro');
+        }
+        if (Notification.permission === 'granted') {
+          const text =
+            timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+          new Notification(text);
+        }
+
+        document.querySelector(`[data-sound="${timer.mode}"]`).play();
+        startTimer();
+    }
+  }, 1000);
+}
+
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
+
+const buttonSound = new Audio('./assets/button-sound.mp3');
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+  buttonSound.play();
+  const { action } = mainButton.dataset;
+  if (action === 'start') {
+  startTimer();
+} else {
+  stopTimer();
+}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  switchMode('pomodoro');
+});
+
+function stopTimer() {
+  clearInterval(interval);
+
+  mainButton.dataset.action = 'start';
+  mainButton.textContent = 'start';
+  mainButton.classList.remove('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Let's check if the browser supports notifications
+  if ('Notification' in window) {
+    // If notification permissions have neither been granted or denied
+    if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+      // ask the user for permission
+      Notification.requestPermission().then(function(permission) {
+        // If permission is granted
+        if (permission === 'granted') {
+          // Create a new notification
+          new Notification(
+            'Awesome! You will be notified at the start of each session'
+          );
+        }
+      });
+    }
+  }
+
+  switchMode('pomodoro');
 });
